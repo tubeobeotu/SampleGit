20b6877829250570125d664c158870969f16ee1c
tunv <>
2020-03-07 12:23:40 +0700

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/minh', function(req, res){
+app.get('/minh123', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
