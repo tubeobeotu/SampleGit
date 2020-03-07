887b57926339ab63fbb5abb23ac5bf92eba91ba8
Swiftaholic <>
2020-03-07 12:23:59 +0700

    abccommit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/minh', function(req, res){
+app.get('/minh321', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat_message', function(msg){
+    io.emit('chat_message', msg);
   });
 });
 
